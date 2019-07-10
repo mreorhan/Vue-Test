7db6550ecd1b7b6353f3fdb6e67531dced208221
mreorhan <videokod7@gmail.com>
2019-07-10 15:58:18 +0300

    Add Vuex

--- src/main.js
+++ src/main.js
@@ -1,13 +1,38 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    todos: [
+      { id: 1, text: 'asasdf', done: true },
+      { id: 2, text: 'asdk', done: false }
+    ]
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done);
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length
+    },
+    getTodoById: (state) => (id) => {
+      return state.todos.find(todo => todo.id === id)
+    }
+  }
+})
+
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   components: { App },
   template: '<App/>'
 })
